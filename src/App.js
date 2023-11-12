9c11ded56a48d26241b6f25d9fbf1f61176948bb
kalyuzhniymaxim <kalyuzhniy.maxim2017@yandex.ru>
2023-11-12 17:34:16 +0300

    test1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { decrement, increment } from './redux/slices/filterSlice'
+
 import Header from './components/Header';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
@@ -13,20 +13,10 @@ export const SearchContext = React.createContext();
 
 function App() {
   const [searchValue, setSearchValue] = useState('');
-  const count = useSelector((state) => state.counter.count);
-  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <div className="wrapper">
-        <div>
-          <button aria-label="Increment value" onClick={() => dispatch(increment())}>
-            Increment
-          </button>
-          <span>{count}</span>
-          <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
-            Decrement
-          </button>
-        </div>
         <SearchContext.Provider value={{searchValue, setSearchValue}}>
           <Header/>
           <div className="content">
