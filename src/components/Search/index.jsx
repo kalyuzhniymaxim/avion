433d2c61d38af16889aa756150b94f52ea01ee0e
kalyuzhniymaxim <kalyuzhniy.maxim2017@yandex.ru>
2023-11-13 19:21:08 +0300

    добавил axios

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,16 +1,45 @@
-import React, { useContext } from 'react';
-import {SearchContext} from '../../App'
+import React, { useCallback, useContext, useEffect, useRef, useState } from 'react';
+import { SearchContext } from '../../App';
+import debounce from 'lodash.debounce';
 import styles from './Search.module.scss';
 import search from './search.png';
 import close from './close.png';
 
 const Search = () => {
-  const {searchValue, setSearchValue} = useContext(SearchContext);
+  const [value, setValue] = useState('');
+
+  const {  setSearchValue } = useContext(SearchContext);
+
+  const inputRef = useRef();
+
+  const onClickClear = () => {
+    setSearchValue('');
+    setValue('');
+    inputRef.current.focus();
+  };
+  const updateSearchValue = useCallback(
+    debounce((str) => {
+      setSearchValue(str);
+    }, 1000),
+    [],
+  );
+
+  const onChangeInput = (event) => {
+    setValue(event.target.value);
+    updateSearchValue(event.target.value);
+  };
+
   return (
     <div className={styles.root}>
       <img className={styles.item} src={search} alt="search" />
-      <input value={searchValue} onChange={(e)=>setSearchValue(e.target.value)} className={styles.input} placeholder="Поиск пицц ..." />
-      {searchValue && <img onClick={() => setSearchValue('')} className={styles.clearIcon} src={close} alt="close" />}
+      <input
+        ref={inputRef}
+        value={value}
+        onChange={onChangeInput}
+        className={styles.input}
+        placeholder="Поиск пицц ..."
+      />
+      {value && <img onClick={onClickClear} className={styles.clearIcon} src={close} alt="close" />}
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,52 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import qs from 'qs';
+import {useNavigate} from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux';
-import { setCategoryId } from '../redux/slices/filterSlice';
+import { setCategoryId, setCurrentPage, setFilters } from '../redux/slices/filterSlice';
 
 import Categories from '../components/Categories';
-import Sort from '../components/Sort';
+import Sort, { list } from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 
 // import '../scss/app.scss';
 
-
 import Pagination from '../components/Pagination';
 import { SearchContext } from '../App';
 
 const Home = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const {categoryId, sort} = useSelector((state) => state.filter);
+  const { categoryId, sort, currentPage } = useSelector((state) => state.filter);
   const sortType = sort.sortProperty;
-
-
 
   const { searchValue } = React.useContext(SearchContext);
   const [isLoading, setIsLoading] = useState(true);
   const [items, setItems] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
+
 
   const onChangeCategory = (id) => {
     dispatch(setCategoryId(id));
   };
 
+  const onChangePage = number =>{
+    dispatch(setCurrentPage(number));
+
+  }
+
+  useEffect(()=>{
+    if(window.location.search){
+      const params = qs.parse(window.location.search.substring(1))
+      const sort = list.find(obj => obj.sortProperty === params.sortProperty)
+      dispatch(
+        setFilters({
+          ...params,
+          sort,
+        })
+      )
+    }
+  },[])
 
   useEffect(() => {
     setIsLoading(true);
@@ -36,16 +54,26 @@ const Home = () => {
     const order = sortType.includes('-') ? 'asc' : 'desc';
     const category = categoryId > 0 ? `category=${categoryId}` : '';
     const search = searchValue ? `&search=${searchValue}` : '';
-    fetch(
-      `https://654c9ff077200d6ba8591d04.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
-    )
-      .then((res) => res.json())
-      .then((arr) => {
-        setItems(arr);
+
+    axios
+      .get(
+        `https://654c9ff077200d6ba8591d04.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
+      )
+      .then((res) => {
+        setItems(res.data);
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
   }, [categoryId, sortType, searchValue, currentPage]);
+
+  useEffect(() => {
+    const quryString =qs.stringify({
+      sortProperty: sort.sortProperty,
+      categoryId,
+      currentPage
+    }) 
+    navigate(`?${quryString}`)
+  }, [categoryId, sortType, searchValue, currentPage])
 
   const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
   const skeletons = [...Array(6)].map((_, index) => <Skeleton key={index} />);
@@ -57,7 +85,7 @@ const Home = () => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">{isLoading ? skeletons : pizzas}</div>
-      <Pagination onChangePage={(n) => setCurrentPage(n)} />
+      <Pagination currentPage={currentPage} onChangePage={onChangePage} />
     </div>
   );
 };
